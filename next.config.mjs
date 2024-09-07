b689c54dbdf446429b32f3b68963e2da82f474c6
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-09-07 16:11:22 +0100

    add mdx support and whitelist plcehold.co

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+      },
+    ],
+  },
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  output: "standalone",
+};
 
 export default nextConfig;
